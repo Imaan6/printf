c3f9d69580aaa43e85b24515adf3669375fb2c2f
Imane El mohalil <elmohalilimane@gmail.com>
2021-12-01 11:42:32 +0100

    printf and friends

---
+++ ft_printf.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-moha <iel-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 15:01:51 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/12/01 11:37:52 by iel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	var;
+	int		i;
+    
+	i = 0;
+    va_start (var, str);
+	while(*str)
+	{
+		if(*str == "%")
+		{
+			str++;
+			if (*str == 'c')
+				i += ft_putchar(va_arg(var, int));
+			else if (*str == 's')
+				i += ft_putstr(va_arg(var, char *));
+			//else if (*str == 'd' || *str == 'i')
+			else if (*str == 'i')
+				i += ft_putnbr(va_arg(var, char *));
+		}
+		else
+			i += ft_putchar(*str);
+		str++;	
+	}
+}
---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-moha <iel-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 15:02:30 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/12/01 11:38:28 by iel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int nb);
+
+#endif
