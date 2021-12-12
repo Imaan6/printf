abffbd38795c664a639d213c7992e3658246f3d8
Imane El mohalil <elmohalilimane@gmail.com>
2021-12-12 14:18:03 +0100

    eeeeeehhhhh

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 11:09:11 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/12/12 13:11:09 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/12/12 14:16:57 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_printf(const char *args, ...)
 			else if (*args == 's')
 				i += ft_putstr(va_arg(ap, char *));
 			else if (*args == 'p')
-				i += ft_putaddress(va_arg(ap , unsigned long));
+				i += ft_putaddress(va_arg(ap, unsigned long));
 			else if (*args == 'd' || *args == 'i')
 				i += ft_putnbr(va_arg(ap, int));
 			else if (*args == 'u')
@@ -42,6 +42,8 @@ int	ft_printf(const char *args, ...)
 			else if (*args == '%')
 				i += ft_putchar('%');
 		}
+		else
+			i += ft_putchar(*args);
 		args++;
 	}
 	va_end(ap);
@@ -51,8 +53,8 @@ int	ft_printf(const char *args, ...)
 #include <stdio.h>
 int	main()
 {
-	int	i;
-	unsigned long a = (unsigned long)&i;
-	printf("%p", &i);
-	ft_printf("%p", &i);
+	int	i = 5;
+//	printf("%lu\n", &i);
+	ft_printf("My printf can print this int %d: and this address : %p", i, &i);
+//	ft_printf("iip \n fg");
 }*/
